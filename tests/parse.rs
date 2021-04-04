fbcaae6e47276a0aa2e709dd63c889302c608746
Sjoerd Simons <sjoerd@collabora.com>
2021-04-04 10:43:17 +0200

    Fix test case; test parser should be 1025 blocks long

--- tests/parse.rs
+++ tests/parse.rs
@@ -5,9 +5,9 @@ fn parse() {
     let xml = include_str!("data/simple.bmap");
     let bmap = Bmap::from_xml(xml).unwrap();
 
-    assert_eq!(4194304, bmap.image_size());
     assert_eq!(4096, bmap.block_size());
-    assert_eq!(1024, bmap.blocks());
+    assert_eq!(1025, bmap.blocks());
+    assert_eq!(1025 * 4096, bmap.image_size());
     assert_eq!(680, bmap.mapped_blocks());
 
     let mut block = 0;
